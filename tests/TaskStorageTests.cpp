a867260874560f3fefbef775f30aea8c9eefc728
Shaqiel <s.f.mohamedhoesein@hotmail.com>
2026-09-25 21:21:43 +0200

    Fixed tests for windows file-handling

--- tests/TaskStorageTests.cpp
+++ tests/TaskStorageTests.cpp
@@ -17,17 +17,19 @@ TEST_CASE("TaskStorage saves tasks to a file")
 
     REQUIRE(std::filesystem::exists(path));
 
-    std::ifstream file{path};
-    REQUIRE(file);
+    {
+        std::ifstream file{path};
+        REQUIRE(file);
 
-    nlohmann::json json;
-    file >> json;
+        nlohmann::json json;
+        file >> json;
 
-    REQUIRE(json.size() == 1);
-    REQUIRE(json.at(0).at("text") == "Study C++");
-    REQUIRE(json.at(0).at("priority") == "HIGH");
-    REQUIRE_FALSE(json.at(0).at("completed").get<bool>());
-    REQUIRE(json.at(0).at("deadline").is_null());
+        REQUIRE(json.size() == 1);
+        REQUIRE(json.at(0).at("text") == "Study C++");
+        REQUIRE(json.at(0).at("priority") == "HIGH");
+        REQUIRE_FALSE(json.at(0).at("completed").get<bool>());
+        REQUIRE(json.at(0).at("deadline").is_null());
+    }
 
     std::filesystem::remove(path);
 }
@@ -37,8 +39,7 @@ TEST_CASE("TaskStorage saves task with deadline correctly")
     const std::chrono::year_month_day deadline{
         std::chrono::year{2026},
         std::chrono::month{9},
-        std::chrono::day{25}
-    };
+        std::chrono::day{25}};
 
     const std::filesystem::path path{"test_tasks.json"};
 
@@ -49,17 +50,19 @@ TEST_CASE("TaskStorage saves task with deadline correctly")
 
     REQUIRE(std::filesystem::exists(path));
 
-    std::ifstream file{path};
-    REQUIRE(file);
+    {
+        std::ifstream file{path};
+        REQUIRE(file);
 
-    nlohmann::json json;
-    file >> json;
+        nlohmann::json json;
+        file >> json;
 
-    REQUIRE(json.size() == 1);
-    REQUIRE(json.at(0).at("text") == "Finish project");
-    REQUIRE_FALSE(json.at(0).at("completed").get<bool>());
-    REQUIRE(json.at(0).at("priority") == "MEDIUM");
-    REQUIRE(json.at(0).at("deadline") == "2026-09-25");
+        REQUIRE(json.size() == 1);
+        REQUIRE(json.at(0).at("text") == "Finish project");
+        REQUIRE_FALSE(json.at(0).at("completed").get<bool>());
+        REQUIRE(json.at(0).at("priority") == "MEDIUM");
+        REQUIRE(json.at(0).at("deadline") == "2026-09-25");
+    }
 
     std::filesystem::remove(path);
 }
@@ -71,29 +74,25 @@ TEST_CASE("TaskStorage saves and loads tasks")
     const std::chrono::year_month_day deadline{
         std::chrono::year{2026},
         std::chrono::month{9},
-        std::chrono::day{25}
-    };
+        std::chrono::day{25}};
 
     std::vector<Task> original_tasks{};
 
     original_tasks.emplace_back(
         "Finish project",
         deadline,
-        Priority::HIGH
-    );
+        Priority::HIGH);
 
     original_tasks.emplace_back(
         "Buy groceries",
-        Priority::MEDIUM
-    );
+        Priority::MEDIUM);
 
     original_tasks.at(0).markComplete();
 
     TaskStorage::save(original_tasks, path);
 
     std::vector<Task> loaded_tasks{
-        TaskStorage::load(path)
-    };
+        TaskStorage::load(path)};
 
     REQUIRE(loaded_tasks.size() == 2);
 
